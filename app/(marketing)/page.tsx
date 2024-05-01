0c668a41613800a9b5e30dcedb2bd6738db530b4
Angiegru <angiegrullon01@gmail.com>
2024-05-01 11:58:18 -0400

    landing page skeleton: (marketing) route group, layout.tsx, header.tsx, footer.tsx created & page.tsx moved

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-    <Button size="lg">Click Me!</Button>
+    This is a marketing page!
    </div>
   );
 }
