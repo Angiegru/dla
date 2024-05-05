88110ec3c90ea85da74d0b56254146d3eb9d9441
Angiegru <angiegrullon01@gmail.com>
2024-05-05 01:07:33 -0400

    Completed Header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import { 
+    ClerkLoaded,
+    ClerkLoading,
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton
+} from '@clerk/nextjs';
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
 export const Header = () => {
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/langLogo.png" height={40} width={40} alt="Logo"/>
+                    <h1 className="text-2xl font-extrabold text-brown-400">
+                        DLA
+                    </h1>
+                </div>
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton
+                            mode="modal"
+                            forceRedirectUrl="/learn"
+                        >
+                            <Button size="lg" variant="ghost">
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
         </header>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,4 +22,4 @@ export default function RootLayout({
       </html>
     </ClerkProvider>
   );
-}
+};
