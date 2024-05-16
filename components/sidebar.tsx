479f64a24beaef900189b57ecdca682188f1393f
Angiegru <angiegrullon01@gmail.com>
2024-05-15 23:18:33 -0400

    'mobile-header.tsx': navbar change to green,'mobile-sidebar.tsx': collapsible in mobile view, 'sidebar.tsx':app icon and name added, name turned into link to '/learn'

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 type Props ={
     className?: string;
@@ -6,10 +8,17 @@ type Props ={
 export const Sidebar = ({ className }: Props) => {
     return (
         <div className= {cn(
-            "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+            "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
         )}>
-            Sidebar
+            <Link href="/learn">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/langLogo.png" height={40} width={40} alt="Logo"/>
+                    <h1 className="text-2xl font-extrabold text-black tracking-wide">
+                        DLA
+                    </h1>
+                </div>
+            </Link>
         </div>
     );
 };
