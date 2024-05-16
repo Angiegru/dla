bd8cf8821bd29896cef562609d6773671e138151
Angiegru <angiegrullon01@gmail.com>
2024-05-16 02:25:13 -0400

    'layout.tsx': text no longer expands with breakpoint, red color removed |'StickyWrapper' and 'FeedWrapper' components created, imported and used in 'page.tsx'

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,14 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { FeedWrapper } from "@/components/feed-wrapper";
 const LearnPage = () => {
     return (
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                My sticky wrapper.
+            </StickyWrapper>
+            <FeedWrapper>
+                My feed wrapper.
+            </FeedWrapper>
         </div>
     );
 }
