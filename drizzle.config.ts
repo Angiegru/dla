467fba37b3b78a292f52bac0eb7c1b027efee834
Angiegru <angiegrullon01@gmail.com>
2024-05-16 23:08:22 -0400

    drizzle install, react version conflicts resolved, scripts added, 'db' folder & 'drizzle.ts' & 'schema.ts' created, drizzle setup & configuration completed

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!, 
+},
+migrations: {
+    table: "custom_migrations_table",
+    schema: "public"
+  }
+});
